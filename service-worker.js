d5e4dfb8e8555f7ea9a84eefac7ccbfc5f376c76
johanschrub-prog <johanschrub@gmail.com>
2026-09-15 14:58:13 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,16 @@
-const CACHE_NAME = "stock-economat-v1";
-
-const filesToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(filesToCache))
-  );
+self.addEventListener('install', () => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
   );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
 });
