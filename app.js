bfc8eb10cf2564a347f0652a791ea4e92c0a9631
johanschrub-prog <johanschrub@gmail.com>
2026-09-18 09:51:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -596,110 +596,16 @@ XLSX.writeFile(
 
 function importExcel(event){
 
+    alert("IMPORT DEMARRE");
+
     const file = event.target.files[0];
 
-    if(!file) return;
+    if(!file){
+        alert("AUCUN FICHIER");
+        return;
+    }
 
-    const reader = new FileReader();
-
-    reader.onload = function(e){
-
-        const workbook =
-        XLSX.read(
-            e.target.result,
-            {type:"array"}
-        );
-
-        let imported = {
-            devant: [],
-            champagne: [],
-            alcool: []
-        };
-
-        function lireFeuille(
-            nomFeuille,
-            destination
-        ){
-
-            const sheet =
-            workbook.Sheets[nomFeuille];
-
-            if(!sheet) return;
-
-            const rows =
-            XLSX.utils.sheet_to_json(sheet);
-
-            rows.forEach(row => {
-
-               destination.push({
-
-  const code =
-    row.code ||
-    row.CODE ||
-    row.Code ||
-    "";
-
-const article =
-    row.article ||
-    row.ARTICLE ||
-    row.Article ||
-    "";
-
-const quantite =
-    row.quantite ??
-    row.QUANTITE ??
-    row.Quantite ??
-    0;
-
-destination.push({
-
-    id: crypto.randomUUID(),
-
-    code: code,
-
-    article: article,
-
-    quantite: Number(quantite)
-
-});
-});
-});
-        }
-
-        lireFeuille(
-            "DEVANT BAR",
-            imported.devant
-        );
-
-        lireFeuille(
-            "ARRIERE BAR CHAMPAGNE",
-            imported.champagne
-        );
-
-        lireFeuille(
-            "ARRIERE BAR ALCOOL",
-            imported.alcool
-        );
-alert(
-"DEVANT=" + imported.devant.length +
-" CHAMPAGNE=" + imported.champagne.length +
-" ALCOOL=" + imported.alcool.length
-);
-        data = imported;
-
-        save();
-
-        render();
-
-     alert(
-    "DEVANT BAR : " + imported.devant.length +
-    "\nCHAMPAGNE : " + imported.champagne.length +
-    "\nALCOOL : " + imported.alcool.length
-);
-
-    };
-
-     reader.readAsArrayBuffer(file);
+    alert("Fichier : " + file.name);
 
 }
 
